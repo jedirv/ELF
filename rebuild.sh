8476e59c4565943139de18b99d28bbf94fb34a92
Jed Irvine <jedirv@gmail.com>
2017-08-20 19:25:27 -0700

    removing script file that was added on wrong branch

--- rebuild.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export CXX=g++
-export CC=gcc
-if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi
-(cd rts/game_MC && make gen && make)
-cd rts/backend
-make clean && make minirts GAME_DIR=../game_MC
